9fd53a773d64a6cd471025e92c802c482a6da8f8
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-04-27 19:43:47 +0800

    Create YoungAdult.java

---
+++ Inheritance 2/YoungAdult.java
@@ -0,0 +1,20 @@
+public class YoungAdult extends Person{
+	
+	int kabit;
+	
+	YoungAdult(String name, String gender, int age,int kabit){
+		super(name,gender,age);
+		this.kabit = kabit;
+	}
+	
+	void drink() {
+		System.out.println(name +" Is Drinking alcohol");
+	}
+	
+	void checkStatus() {
+		super.checkStatus();
+		System.out.println("kabit counts : " + kabit);
+		
+	}
+
+}
