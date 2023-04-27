0dc438be2ac8fe15e69c68dfa92d6bd7312c97bf
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-04-27 19:43:07 +0800

    Create Minor.java

---
+++ Inheritance 2/Minor.java
@@ -0,0 +1,14 @@
+public class Minor extends YoungAdult{
+	
+	int gradeLevel;
+	
+	Minor(String name, String gender, int age, int kabit, int gradeLevel){
+		super(name,gender,age,kabit);
+		this.gradeLevel = gradeLevel;
+	}
+	
+	void sayGradeLevel() {
+		System.out.println("I'am grade : " + gradeLevel);
+	}
+
+}
