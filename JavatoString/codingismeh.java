312fda1805cbf58454e092c0abca447781d9a7bb
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-04-27 20:25:18 +0800

    Create codingismeh.java

---
+++ JavatoString/codingismeh.java
@@ -0,0 +1,11 @@
+public class codingismeh{
+    public static void main(String[] args) {
+        food f = new food(175, "Pesto Pasta");
+        food f2 = new food(80, "Banana Shake");
+        food f3 = new food(95, "Fries & Mojos");
+
+        System.out.println(f.toString());
+        System.out.println(f2.toString());
+        System.out.println(f3.toString());
+    }
+}
