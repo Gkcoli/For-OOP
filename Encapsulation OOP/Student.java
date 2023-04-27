79a545148e7b6a52f3913674f8c0e97499a52a31
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-04-27 20:11:47 +0800

    Create Student.java

---
+++ Encapsulation OOP/Student.java
@@ -0,0 +1,25 @@
+ public class Student{
+
+    String name;
+    int age; 
+    boolean kabit;
+
+    public void setName(String newName){
+        name = newName;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int newAge){
+        age = newAge;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void mayKabit(boolean merongKabit){
+        kabit = merongKabit;
+    }
+    public boolean haveKabit(){
+        return kabit;
+    }
+ }
