7c667971d9266e1c0bb65702dbaca6d09916edd2
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-05-15 17:09:06 +0800

    Create Box.java

---
+++ Machine Problem 1/Classpack/Box.java
@@ -0,0 +1,17 @@
+package ClassPack;
+public class Box extends Rectangle {
+   
+    private double height;
+
+    public Box(double length, double width, double height){
+       super(length, width);
+       this.height = height;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public double getVolume(){
+        return getArea()* height;
+    }
+    
+}
