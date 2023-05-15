a90d9c7eaecc134d747e99140d25f8d48d79b760
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-05-15 16:57:18 +0800

    Create Rectangle.java

---
+++ Machine Problem 2/ClassPack/Rectangle.java
@@ -0,0 +1,16 @@
+package ClassPack;
+
+public class Rectangle extends Shape {
+  
+    public Rectangle(double height, double width){
+    super(height, width);
+    
+  }
+  public double getArea(){
+    return getHeight() * getWidth();
+}
+
+
+    
+}
+
